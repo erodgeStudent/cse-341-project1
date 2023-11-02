d47a7dc46c07654466334492a611b6c6dfca2648
EMERY RODZIEWICZ <rod20012@byui.edu>
2023-11-02 12:08:22 -0400

    get single is not working added routes.rest file. only first send request is showing up

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
+
 
 
 mongodb.initDb((err) => {
@@ -15,8 +17,4 @@ mongodb.initDb((err) => {
     } else {
         app.listen(port, () => {console.log(`Database is listening and running on port ${port}`)});
     }
-})
-
-
-
-
+});
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
- const express = require('express');
+const express = require('express');
  const router = express.Router();
 
  const usersController = require('../controllers/users');
 
  router.get('/', usersController.getAll);
  router.get('/', usersController.getSingle); 
+
+ //3 more endpoints
+ router.post('/', usersController.createUser);
+ router.put('/:id', usersController.updateUser); //could also use patch
+ router.delete('/id', usersController.deleteUser);
+
 
  module.exports = router;
